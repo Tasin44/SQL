218a898a9210cc6216ab46c231913b54fc6f255e
Tasin Mahmud <tasinmahmud44@gmail.com>
2024-08-30 22:59:40 +0600

    22.leetcode_1193. Monthly Transactions I.sql

--- 22.leetcode_1193. Monthly Transactions I.sql
+++ 22.leetcode_1193. Monthly Transactions I.sql
@@ -75,3 +75,40 @@ For all other rows, 0 is added, effectively ignoring declined transactions in th
 
 */
 
+
+#approach 2:
+select left(trans_date,7) as month , 
+country , 
+count(*) as trans_count ,
+sum(state = 'approved') as approved_count ,
+sum(amount) as trans_total_amount ,
+sum((state='approved')*amount) as approved_total_amount
+from Transactions
+group by month,country;
+-- we must be group by both month & country
+
+/*
+Approach
+
+Extract Month and Year:
+Use the LEFT function to get the YYYY-MM part from the trans_date. The LEFT() function extracts a number of characters from a string (starting from left).
+
+Group By Month and Country:
+    Group the transactions by the extracted month and country.
+
+Count Transactions:
+    Use COUNT(id) to count all transactions per group.
+
+Count Approved Transactions:
+    Use SUM(state = 'approved') to count approved transactions, leveraging the fact that boolean expressions return 1 for true and 0 for false.
+
+Sum Total Amounts:
+    Use SUM(amount) to sum the transaction amounts for all transactions per group.
+
+Sum Approved Amounts:
+    Use SUM((state = 'approved') * amount) to sum the transaction amounts for approved transactions, ensuring only approved amounts are summed.
+
+*/
+
+
+
