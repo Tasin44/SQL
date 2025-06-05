59f05a515b76294d3898d3ed06516cb7b0286252
Tasin Mahmud <tasinmahmud44@gmail.com>
2025-06-05 15:00:51 +0600

    6.leetcode_1148_Article Views I.sql

--- 6.leetcode_1148_Article Views I.sql
+++ 6.leetcode_1148_Article Views I.sql
@@ -10,10 +10,36 @@ the author_id and return only one row for each unique author_id.
 */
 
 -- 2nd approach 
-select distinct author_id as id
-from Views
-where author_id = viewer_id
-order by author_id asc;
+SELECT DISTINCT author_id AS id
+FROM views
+WHERE author_id = viewer_id
+ORDER BY id; -- This works because ORDER BY can use aliases
+
+'''
+we can use alias with ''ORDER BY '' but not with ''SELECT '',if we try to do like
+  select distinct(author_id) as id
+  from views
+  where author_id=viewer_id
+  order by id asc;
+
+well get error. because 
+  Because SQL evaluates the WHERE clause before the SELECT clause, the alias id (which is defined in SELECT) does not exist yet when the WHERE clause is being executed.
+
+    ❗ Aliases defined in the SELECT clause cant be used in WHERE.
+  
+'''
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- or we can enclose distinct with bracket 
 
@@ -21,5 +47,28 @@ select distinct(author_id) as id
 from Views
 where author_id = viewer_id
 order by author_id asc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
